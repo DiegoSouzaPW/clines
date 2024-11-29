833dd117d11024a34a079cba3edc0039712014f5
Diego Souza <diegosouza.pw@outlook.com>
2024-11-29 16:44:14 -0300

    inicial

---
+++ src/shared/WebviewMessage.ts
@@ -0,0 +1,33 @@
+import { ApiConfiguration, ApiProvider } from "./api"
+
+export interface WebviewMessage {
+	type:
+		| "apiConfiguration"
+		| "customInstructions"
+		| "alwaysAllowReadOnly"
+		| "webviewDidLaunch"
+		| "newTask"
+		| "askResponse"
+		| "clearTask"
+		| "didShowAnnouncement"
+		| "selectImages"
+		| "exportCurrentTask"
+		| "showTaskWithId"
+		| "deleteTaskWithId"
+		| "exportTaskWithId"
+		| "resetState"
+		| "requestOllamaModels"
+		| "requestLmStudioModels"
+		| "openImage"
+		| "openFile"
+		| "openMention"
+		| "cancelTask"
+		| "refreshOpenRouterModels"
+	text?: string
+	askResponse?: ClineAskResponse
+	apiConfiguration?: ApiConfiguration
+	images?: string[]
+	bool?: boolean
+}
+
+export type ClineAskResponse = "yesButtonClicked" | "noButtonClicked" | "messageResponse"
